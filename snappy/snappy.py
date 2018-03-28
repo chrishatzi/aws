305cfcd094ba74c4a464b25a62811b9d1e6773d3
Chris Hatzikokolakis <chris.hatzikokolakis@gmail.com>
2018-03-28 09:57:12 -0700

    add snapshot functionality

--- snappy/snappy.py
+++ snappy/snappy.py
@@ -4,16 +4,65 @@ import click
 session = boto3.Session(profile_name='default')
 ec2 = session.resource('ec2')
 
-'''
 @click.group()
-@click.option('--debug', default=False, help='turn debug on')
-def cli(debug):
+def cli():
     pass
 
-@cli.command()
-def list_volumes():
-    "List Volumes"
+@cli.group('snapshots')
+def snapshots():
+    '''Commands for snapshots'''
+
 '''
+This command lists out snapshots for the EC2 volumes optionally filtering
+them by the a tag named 'Project'
+'''
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help='Only snapshots for project (tag Project:<name>)')
+def list_volumes(project):
+    '''List EC2 snapshots'''
+
+    instances = get_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(', '.join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime('%c'))))
+
+    return
+
+@cli.group('volumes')
+def volumes():
+    '''Commands for volumes'''
+
+'''
+This command lists out the EC2 instances optionally filtering them by the
+a tag named 'Project'
+'''
+@volumes.command('list')
+@click.option('--project', default=None,
+    help='Only volumes for project (tag Project:<name>)')
+def list_volumes(project):
+    '''List EC2 volumes'''
+
+    instances = get_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(', '.join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + 'GiB',
+                v.encrypted and "Encrypted" or "Not Encrypted")))
+
+    return
 
 '''
 Helper function that returns a list of instances based on an optional filter
@@ -29,9 +78,9 @@ def get_instances(project):
 
     return instances
 
-@click.group()
+@cli.group()
 def instances():
-    'Commands for instances'
+    '''Commands for instances'''
 
 '''
 This command stops EC2 instances optionally filtering them by the
@@ -41,7 +90,7 @@ a tag named 'Project'
 @click.option('--project', default=None,
     help='Only instances for project (tag Project:<name>)')
 def start_instances(project):
-    'Start EC2 instances'
+    '''Start EC2 instances'''
 
     instances = get_instances(project)
 
@@ -59,7 +108,7 @@ a tag named 'Project'
 @click.option('--project', default=None,
     help='Only instances for project (tag Project:<name>)')
 def stop_instances(project):
-    'Stop EC2 instances'
+    '''Stop EC2 instances'''
 
     instances = get_instances(project)
 
@@ -70,29 +119,22 @@ def stop_instances(project):
     return
 
 '''
-This command lists out the EC2 instances optionally filtering them by the
-a tag named 'Project'
+This command creates a snapshot of an EC2 volume optionally filtering them
+by the a tag named 'Project'
 '''
-@instances.command('list')
+@instances.command('snapshot')
 @click.option('--project', default=None,
     help='Only instances for project (tag Project:<name>)')
-def list_instances(project):
-    'List EC2 instances'
+def create_snapshots(project):
+    '''Create snapshots for EC2 instances'''
 
     instances = get_instances(project)
 
     for i in instances:
-        tags = { t['Key']: t['Value'] for t in i.tags or [] }
-
-        print(', '.join((
-            i.id,
-            i.instance_type,
-            i.placement['AvailabilityZone'],
-            i.state['Name'],
-            i.public_dns_name,
-            tags.get('Project', '<no project>'))))
+        for v in i.volumes.all():
+            print('Creating snapshot of {0}'.format(v.id))
 
     return
 
 if __name__ == '__main__':
-    instances()
+    cli()
